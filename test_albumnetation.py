e2b6dcee8adb05e568f4558fb50179f9db16ea09
ngthanhtinqn <ngthanhtinqn@gmail.com>
2023-05-24 11:27:44 -0500

    test mixup without hesitation

--- test_albumnetation.py
+++ test_albumnetation.py
@@ -139,3 +139,125 @@ visualize_2(
     random_bboxes,
 )
 # %%
+import os
+import sys
+import time
+import math
+
+import torch.nn as nn
+import torch.nn.init as init
+
+import numpy as np
+import torch
+
+
+def mixup_data(x, y, alpha=1.0, device='cpu'):
+
+    '''Compute the mixup data. Return mixed inputs, pairs of targets, and lambda'''
+    if alpha > 0.:
+        lam = np.random.beta(alpha, alpha)
+        lam = max(lam, 1-lam)
+        # lam = min(lam, 1-lam)
+    else:
+        lam = 1.
+    batch_size = x.size()[0]
+ 
+    index = torch.randperm(batch_size).to(device)
+
+
+    ## SYM
+    # mixed_x = lam * x + (1 - lam) * x[index,:]
+    # mixed_y = (1 - lam) * x + lam * x[index,:]
+    # mixed_image  = torch.cat([mixed_x,mixed_y], 0)
+    # y_a, y_b = y, y[index]
+    # mixed_label  = torch.cat([y_a,y_b], 0)
+
+
+    ## Reduce batch size
+    # new_batch_size = batch_size // 2
+    # x_i = x[ : new_batch_size]
+    # x_j = x[new_batch_size : ]
+    # y_a = y[ : new_batch_size]
+    # y_b = y[new_batch_size : ]
+    # mixed_x = lam * x_i + (1 - lam) * x_j
+
+    ## NO SYM
+    mixed_x = lam * x + (1 - lam) * x[index,:]
+    y_a, y_b = y, y[index]
+
+    ## Only Alpha
+    # mixed_x = 0.5 * x + (1 - 0.5) * x[index,:]
+    # mixed_image  = mixed_x
+    # y_a, y_b = y, y[index]
+    # ind_label = torch.randint_like(y, 0,2)
+    # mixed_label  = ind_label * y_a + (1-ind_label) * y_b
+
+    ## Reduce batch size and SYM
+    # new_batch_size = batch_size // 2
+    # x_i = x[ : new_batch_size]
+    # x_j = x[new_batch_size : ]
+    # y_a = y[ : new_batch_size]
+    # y_b = y[new_batch_size : ]
+    # mixed_x = lam * x_i + (1 - lam) * x_j
+    # mixed_y = (1 - lam) * x_i + lam * x_j
+    # mixed_x  = torch.cat([mixed_x,mixed_y], 0)
+    # y_b = torch.cat([y_b,y_a], 0)
+    # y_a = y
+
+
+    # return mixed_image, mixed_label, lam
+    return mixed_x, y_a, y_b, lam
+
+def mixup_criterion(y_a, y_b, lam):
+    # sigmoid = 1.0/(1 + math.exp( 5 - 10*lam))
+    # sigmoid = 4.67840515/(5.85074311 + math.exp(6.9-10.2120858*lam))
+    # sigmoid = 1.531 /(1.71822 + math.exp(6.9-12.2836*lam))
+    # return lambda criterion, pred: sigmoid * criterion(pred, y_a) + (1 - sigmoid) * criterion(pred, y_b)
+
+    return lambda criterion, pred: lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
+
+# %%
+import cv2
+import torchvision.transforms as transforms
+from matplotlib import pyplot as plt
+
+def show_image(image):
+    """
+    image: (H,W, 3)
+    """
+    # normalize to [0-1]
+    image = (image - image.min())/(image.max() - image.min())        
+    plt.imshow(image, cmap='bone')    
+        
+    plt.axis('off')
+
+image1 = cv2.imread("/home/tin/deforestation/dataset/processed/visibles/9684487/composite.png")
+image2 = cv2.imread("/home/tin/deforestation/dataset/processed/visibles/3418608/composite.png")
+image3 = cv2.imread("/home/tin/deforestation/dataset/processed/visibles/3847468/composite.png")
+image4 = cv2.imread("/home/tin/deforestation/dataset/processed/visibles/3915278/composite.png")
+image5 = cv2.imread("/home/tin/deforestation/dataset/processed/visibles/14353252/composite.png")
+
+
+transform = transforms.ToTensor()
+
+tensor1 = transform(image1)
+tensor2 = transform(image2)
+tensor3 = transform(image3)
+tensor4 = transform(image4)
+tensor5 = transform(image5) 
+
+# %%
+x = torch.stack([tensor1, tensor2, tensor3, tensor4, tensor5], dim=0)
+y = torch.zeros(5)
+mixed_x, y_a, y_b, lam = mixup_data(x, y, 0.5, device='cpu')
+y_a, y_b, mixed_x.shape
+
+
+# %%
+for i in range(5):
+    image = mixed_x[i]
+    image = torch.permute(image, (2,1,0))
+    image = np.copy(image)
+    show_image(image)
+    plt.show()
+# %%
